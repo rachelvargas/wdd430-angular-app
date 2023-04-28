ebf912f4e95a0f9d4f3d77283115fd5fad7314d0
Rachel Vargas <angeles.vargas@ymail.com>
2023-04-28 16:04:32 -0700

    I added the cms project

---
+++ cms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent {
+
+}
